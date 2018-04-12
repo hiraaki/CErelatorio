f82f54c5303116c0c8e4f517a40a671a33381b3c
Hiraaki <mauricio_ishida@hotmail.com>
2018-04-12 13:32:39 -0300

    testes

--- src/Main.java
+++ src/Main.java
@@ -14,21 +14,78 @@ public class Main {
 
 
     public static void main(String[] args){
-        Populacao pop = new Populacao(30,5,-5);
+        Populacao pop = new Populacao(20,5,-5);
         //pop.printPopulaca();
         //System.out.println("\n\n----------------------------------------------");
-        //onePlusOne(pop,5000,-0.1,0.1);
-        miplusmi(pop,50,-0.1,0.1);
-        pop.individuos.clear();
-        pop = new Populacao(20,5,-5);
-        metaEvolucianario(pop,50,-0.1,0.1);
-        //localSearch(200,5000,-5,5,0.1,1);
+        Individuos result= new Individuos();
+        double x=0;
+        double y=0;
+//        double resp=0;
+        int resp=0;
+        //media para 1+1
+        for(int i=0;i<10;i++){
+            pop.individuos.clear();
+            pop=new Populacao(20,5,-5);
+//            result=onePlusOne(pop,80,-0.1,0.1);
+            resp+=onePlusOne(pop,50,-0.1,0.1);
+//            x+=result.x;
+//            y+=result.y;
+//            resp+=result.resp;
+        }
+        //System.out.println(x/10+" "+y/10+" "+resp/10);
+        System.out.println(resp/10);
+        System.out.println("----------------------------");
+        //m�dia para mi+mi
+        x=0;y=0;resp=0;result= new Individuos();
+        for(int i=0;i<10;i++){
+            pop.individuos.clear();
+            pop=new Populacao(20,5,-5);
+//            result=miplusmi(pop,20,-0.1,0.1);
+              resp+=miplusmi(pop,50,-0.1,0.1);
+//            x+=result.x;
+//            y+=result.y;
+//            resp+=result.resp;
+        }
+//        System.out.println(x/10+" "+y/10+" "+resp/10);
+        System.out.println(resp/10);
+        System.out.println("----------------------------");
+        x=0;y=0;resp=0;result= new Individuos();
+        for(int i=0;i<10;i++){
+            pop.individuos.clear();
+            pop=new Populacao(20,5,-5);
+            result=metaEvolucianario(pop,20,-0.1,0.1);
+            x+=result.x;
+            y+=result.y;
+            resp+=result.resp;
+        }
+        System.out.println(x/10+" "+y/10+" "+resp/10);
+        System.out.println("----------------------------");
+        x=0;y=0;resp=0;result= new Individuos();
+        for(int i=0;i<10;i++){
+            pop.individuos.clear();
+            pop=new Populacao(20,5,-5);
+            result=localSearch(50,70,-5,5,0.05,1);
+            x+=result.x;
+            y+=result.y;
+            resp+=result.resp;
+        }
+        System.out.println(x/10+" "+y/10+" "+resp/10);
+        System.out.println("----------------------------");
+
+//        pop.individuos.clear();
+//        pop = new Populacao(20,5,-5);
+//        miplusmi(pop,50,-0.1,0.1);
+//        pop.individuos.clear();
+//        pop = new Populacao(20,5,-5);
+//        metaEvolucianario(pop,50,-0.1,0.1);
+//        localSearch(20,50,-5,5,0.1,1);
 
 
 
 
     }
-    public static void  onePlusOne(Populacao p,int geracao, double ini, double fim){
+    public static int  onePlusOne(Populacao p,int geracao, double ini, double fim){
+//    public static Individuos  onePlusOne(Populacao p,int geracao, double ini, double fim){
 
         Individuos oMelhor=new Individuos();
         ArrayList<Populacao> geracoes= new ArrayList<>();
@@ -54,8 +111,8 @@ public class Main {
                 nova.individuos.add(modificado);
 
             }
-            nova.printPopulaca();
-            System.out.println("----------------------------------------------");
+            //nova.printPopulaca();
+            //System.out.println("----------------------------------------------");
 
             Individuos resp=new Individuos(0,0,Double.MIN_VALUE);
             int id=0,ic=0;
@@ -66,18 +123,30 @@ public class Main {
                 }
                 ic++;
             }
-            System.out.println(id+" "+resp.x+" "+resp.y+" "+resp.resp);
-            System.out.println("\n\n----------------------------------------------");
+            //System.out.println(id+" "+resp.x+" "+resp.y+" "+resp.resp);
+            //System.out.println("\n\n----------------------------------------------");
             geracoes.add(nova);
-            if(resp.resp>oMelhor.resp){
-                oMelhor=resp;
-                g=i;
+            /*if para considerar até a minima mudança*/
+//            if (resp.resp > oMelhor.resp) {
+//                oMelhor = resp;
+//                g = i;
+//            }
+            /*if para controle de aperfeçoamento de resposta*/
+            if(Math.abs(resp.resp-oMelhor.resp)>0.00001) {
+                if (resp.resp > oMelhor.resp) {
+                    oMelhor = resp;
+                    g = i;
+                }
+            }else if(Math.abs(resp.resp-oMelhor.resp)<0.00001){
+//                System.out.println("----------------------------------------------");
+                System.out.println(resp.x+" "+resp.y+" "+resp.resp);
+                break;
             }
         }
-        System.out.println(geracoes.size());
-        System.out.println(g+" "+oMelhor.x+" "+oMelhor.y+" "+oMelhor.resp);
 
-
+        //System.out.println(g+" "+oMelhor.x+" "+oMelhor.y+" "+oMelhor.resp);
+        return g;
+        //return oMelhor;
     }
 
     /*
@@ -87,11 +156,18 @@ public class Main {
     * e os melhores da anterior � a que cria uma nova eraç�o
     *
     * */
-    public static void miplusmi(Populacao p,int geracao, double ini, double fim){
+    //public static Individuos miplusmi(Populacao p,int geracao, double ini, double fim){
+    public static int miplusmi(Populacao p,int geracao, double ini, double fim){
 
         Populacao q = new Populacao();
+        Individuos resp = new Individuos();
+        Individuos oMelhor = new Individuos();
+        Collections.sort(p.individuos);
+        oMelhor.x=p.individuos.get(p.individuos.size()-1).x;
+        oMelhor.y=p.individuos.get(p.individuos.size()-1).y;
+        oMelhor.resp=p.individuos.get(p.individuos.size()-1).resp;
+        int g=0;
         for(int j=0;j<geracao;j++) {
-            Collections.sort(p.individuos);
             for (int i = 0; i < p.individuos.size(); i++) {
                 Individuos ind = new Individuos();
                 ind.x = p.individuos.get(i).x + doubleAleatorio(ini, fim);
@@ -103,22 +179,40 @@ public class Main {
             for(int i=0;i<p.individuos.size()/2+1;i++){
                 p.individuos.set(i,q.individuos.get(q.individuos.size()-i-1));
             }
-            System.out.println("\n\n----------------------------------------------");
-            p.printPopulaca();
+            Collections.sort(p.individuos);
+            resp.x=p.individuos.get(p.individuos.size()-1).x;
+            resp.y=p.individuos.get(p.individuos.size()-1).y;
+            resp.resp=p.individuos.get(p.individuos.size()-1).resp;
+
+            if((Math.abs(resp.resp-oMelhor.resp)<0.00001)&&(resp.resp!=oMelhor.resp)||(j>(geracao*0.8))) {
+                //System.out.println("\n\n----------------------------------------------");
+                //p.printPopulaca();
+                //System.out.println(oMelhor.x+" "+oMelhor.y+" "+oMelhor.resp);
+                System.out.println(resp.x+" "+resp.y+" "+resp.resp);
+                break;
+            }
+            if (resp.resp > oMelhor.resp) {
+                oMelhor.x=resp.x;
+                oMelhor.y=resp.y;
+                oMelhor.resp=resp.resp;
+                g = j;
+
+            }
         }
-
-        System.out.println("\n\n----------------------------------------------");
-        Collections.sort(p.individuos);
-        p.printPopulaca();
-        System.out.println("----------------------------------------------\n\n");
-
+        //return oMelhor;
+        return g;
     }
 
-    public static void metaEvolucianario(Populacao p,int geracao, double ini, double fim){
-
+    public static Individuos metaEvolucianario(Populacao p,int geracao, double ini, double fim){
+        Individuos resp = new Individuos();
+        Individuos oMelhor = new Individuos();
         Populacao q = new Populacao();
+        int g=0;
+        Collections.sort(p.individuos);
+        oMelhor.x=p.individuos.get(p.individuos.size()-1).x;
+        oMelhor.y=p.individuos.get(p.individuos.size()-1).y;
+        oMelhor.resp=p.individuos.get(p.individuos.size()-1).resp;
         for(int j=0;j<geracao;j++) {
-            Collections.sort(p.individuos);
             for (int i = 0; i < p.individuos.size(); i++) {
                 Individuos ind = new Individuos();
                 ind.x = p.individuos.get(i).x + p.var();
@@ -130,24 +224,49 @@ public class Main {
             for(int i=0;i<p.individuos.size()/2+1;i++){
                 p.individuos.set(i,q.individuos.get(q.individuos.size()-i-1));
             }
-            System.out.println("\n\n----------------------------------------------");
-            p.printPopulaca();
+            //System.out.println("\n\n----------------------------------------------");
+            //p.printPopulaca();
+            Collections.sort(p.individuos);
+            resp.x=p.individuos.get(p.individuos.size()-1).x;
+            resp.y=p.individuos.get(p.individuos.size()-1).y;
+            resp.resp=p.individuos.get(p.individuos.size()-1).resp;
+            if((Math.abs(resp.resp-oMelhor.resp)<0.00001)&&(resp.resp!=oMelhor.resp)||(j>(geracao*0.8))) {
+                //System.out.println("\n\n----------------------------------------------");
+                //p.printPopulaca();
+                System.out.println(oMelhor.x+" "+oMelhor.y+" "+oMelhor.resp);
+                System.out.println(resp.x+" "+resp.y+" "+resp.resp);
+                break;
+            }
+            if (resp.resp > oMelhor.resp) {
+                oMelhor.x=resp.x;
+                oMelhor.y=resp.y;
+                oMelhor.resp=resp.resp;
+                g = j;
+
+            }
         }
 
-        System.out.println("\n\n----------------------------------------------");
-        Collections.sort(p.individuos);
-        p.printPopulaca();
+//        System.out.println("\n\n----------------------------------------------");
+//        Collections.sort(p.individuos);
+//        p.printPopulaca();
 
+
+//
+        return oMelhor;
     }
-    public static void localSearch(int interacoes,int geracao, double ini, double fim,double exploracao,double exportacao){
+    public static Individuos localSearch(int interacoes,int geracao, double ini, double fim,double exploracao,double exportacao){
         Populacao p=new Populacao();
         Individuos Best=new Individuos();
         Best.x=doubleAleatorio(ini,fim);
         Best.y=doubleAleatorio(ini,fim);
         Best.resp=p.fun(Best.x,Best.y);
+        double x=0;
+        double y=0;
         Individuos best=new Individuos(Best.x,Best.y,Best.resp);
         for(int i=0; i< geracao;i++){
             for(int j=0;j<interacoes;j++){
+                x=best.x;
+                y=best.y;
                 best.x+=doubleAleatorio(-exploracao,exploracao);
                 best.y+=doubleAleatorio(-exploracao,exploracao);
                 while (best.x>fim||best.x<ini)
@@ -155,15 +274,20 @@ public class Main {
                 while (best.y>fim||best.y<ini)
                     best.y+=doubleAleatorio(-exploracao,exploracao);
                 best.resp=p.fun(best.x,best.y);
+                if(p.fun(x,y)>best.resp) {
+                    best.x=x;
+                    best.y=y;
+                    best.resp=p.fun(x,y);
+                }
                 if(best.resp>Best.resp) {
                     Best.x = best.x;
                     Best.y = best.y;
                     Best.resp = best.resp;
                 }
             }
-            System.out.println("\n\n--------------------------");
-            System.out.println(best.x+" "+best.y+" "+best.resp);
-            System.out.println(Best.x+" "+Best.y+" "+Best.resp);
+            //System.out.println("\n\n--------------------------");
+            //System.out.println(best.x+" "+best.y+" "+best.resp);
+            //System.out.println(Best.x+" "+Best.y+" "+Best.resp);
             best.x+=doubleAleatorio(-exportacao,exportacao);
             best.y+=doubleAleatorio(-exportacao,exportacao);
             while (best.x>fim||best.x<ini)
@@ -172,8 +296,9 @@ public class Main {
                 best.y+=doubleAleatorio(-exportacao,exportacao);
             best.resp=p.fun(best.x,best.y);
         }
-        System.out.println("\n\n\n--------------------------");
-        System.out.println(Best.x+" "+Best.y+" "+Best.resp);
+//        System.out.println("\n\n\n--------------------------");
+//        System.out.println(Best.x+" "+Best.y+" "+Best.resp);
+        return Best;
     }
 
 
